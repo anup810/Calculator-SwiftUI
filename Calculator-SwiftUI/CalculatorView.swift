cc108abc549bf81b98471d75d97095cbe41432e4
Anup Saud <anupsaud10@gmail.com>
2025-03-17 17:24:42 -0400

    Inital project structure

---
+++ Calculator-SwiftUI/CalculatorView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calculator-SwiftUI
+//
+//  Created by Anup Saud on 2025-03-17.
+//
+
+import SwiftUI
+
+struct CalculatorView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    CalculatorView()
+}
---
+++ Calculator-SwiftUI/Calculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_SwiftUIApp.swift
+//  Calculator-SwiftUI
+//
+//  Created by Anup Saud on 2025-03-17.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CalculatorView()
+        }
+    }
+}
